dddbd3ecc00d821ecbd28ac68c5b087841d0a781
Andrew Minion <andrew@andrewrminion.com>
2015-08-20 20:55:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Introduction
+
+A simple plugin to show featured images after the post title on single pages. That’s it.
---
+++ genesis-post-show-featured-image.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: GHC Functionality
+ * Plugin URI: https://github.com/macbookandrew/genesis-post-show-featured-image
+ * Description: Show post featured image after the title
+ * Version: 1.0
+ * Author: AndrewRMinion Design
+ * Author URI: http://andrewrminion.com
+ * Copyright: 2015 AndrewRMinion Design (andrew@andrewrminion.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( 'genesis_after_post_title', 'featured_post_image', 8 );
+function featured_post_image() {
+  if ( ! is_singular( 'post' ) )  return;
+	the_post_thumbnail('post-image');
+}
